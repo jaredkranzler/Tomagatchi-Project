8b29678fa00e56b4585b4d82bd2ff2b86e0f21c7
Jared Kranzler <kranzler312@gmail.com>
2018-06-26 14:30:10 -0500

    got the buttons to count down on click

--- js/app.js
+++ js/app.js
@@ -1,10 +1,54 @@
 console.log('Tomagotchichichichi');
 
+// class---
+class Tom {
+  constructor(name, age, hungerLevel , sleepiness, boredom){
+    this.name = name;
+    this.age = age;
+    this.hungerLevel = hungerLevel;
+    this.sleepiness = sleepiness;
+    this.boredom = boredom;
+  }
+     eat(food){
+      if (this.hungerLevel == 10); {
+      window.alert("You Killed It, Literally! You Are Dead")
+      }
+     }
+     sleepMore(){
+      if (this.sleepiness === 10) {
+        // window.alert("Your Dead Tired, But really, Your Dead!")
+      }
+     }
+     playMore(){
+      this.boredom--
+      if (this.boredom === 10) {
+        window.alert("You've Had Your Fun, Now Your Dead!")
+      }
+     }
+};
+
+const xxx = new Tom('Jim', 0, 0, 0, 0)
+
+// Clicking -----
+
+$('.F').on('click', (e) => {
+  console.log('clicked food');
+  // const food = $(e.currentTarget);
+  xxx.eat();
+  $('#hunger').text("Hunger:( " + xxx.hungerLevel++ + " )");
+}); 
 
 
+$('.L').on('click', (e) => {
+  // const $clickedDiv = $(e.currentTarget);
+  $('#sleep').text("Sleepiness:( " + xxx.sleepiness-- + " )");
+});
 
 
-
+$('.P').on('click', (e) => {
+  // const $clickedDiv = $(e.currentTarget);
+  $('#play').text("Boredom:( " + xxx.boredom++ + " )")
+});
 
 
 
