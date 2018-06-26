b62b7cbfdd972be25a05f20f023b5b7c32980ead
Jared Kranzler <kranzler312@gmail.com>
2018-06-26 16:21:31 -0500

    button working for real this time /

--- js/app.js
+++ js/app.js
@@ -1,56 +1,68 @@
 console.log('Tomagotchichichichi');
 
 // class---
+let time = 30
 class Tom {
-  constructor(name, age, hungerLevel , sleepiness, boredom){
+  constructor(name, age, hungerLevel, sleepiness, boredom, time){
     this.name = name;
+    this.time = 0;
     this.age = age;
     this.hungerLevel = hungerLevel;
     this.sleepiness = sleepiness;
     this.boredom = boredom;
   }
-     eat(food){
-      if (this.hungerLevel == 10); {
-      window.alert("You Killed It, Literally! You Are Dead")
-      }
-     }
-     sleepMore(){
-      if (this.sleepiness === 10) {
-        // window.alert("Your Dead Tired, But really, Your Dead!")
-      }
-     }
-     playMore(){
-      this.boredom--
-      if (this.boredom === 10) {
-        window.alert("You've Had Your Fun, Now Your Dead!")
-      }
-     }
-};
+  eat(food){
+    this.hungerLevel--
+    $('#hunger').text("Hunger:( " + this.hungerLevel + " )");
 
+  }
+
+  sleepMore(){
+    this.sleepiness--
+    $('#sleep').text("Rest:( " + this.sleepiness + " )");
+      // window.alert("Your Dead Tired, But really, Your Dead!")
+    
+  }
+  playMore(){
+   this.boredom--
+   $('#play').text("Boredom:( " + this.boredom-- + " )")
+  }
+};
 const xxx = new Tom('Jim', 0, 0, 0, 0)
 
 // Clicking -----
 
 $('.F').on('click', (e) => {
   console.log('clicked food');
-  // const food = $(e.currentTarget);
-  xxx.eat();
-  $('#hunger').text("Hunger:( " + xxx.hungerLevel++ + " )");
+  xxx.eat()
 }); 
 
 
 $('.L').on('click', (e) => {
-  // const $clickedDiv = $(e.currentTarget);
-  $('#sleep').text("Sleepiness:( " + xxx.sleepiness-- + " )");
+  xxx.sleepMore();
 });
 
 
 $('.P').on('click', (e) => {
-  // const $clickedDiv = $(e.currentTarget);
-  $('#play').text("Boredom:( " + xxx.boredom++ + " )")
+  xxx.playMore()
 });
 
+// xxx.eat();
 
 
+// set interval
 
+  // increase hunger
 
+/// timer code goes here
+    //     const intervalId = setInterval(() =>{
+    //       this.time;
+    //       if(time % 5 === 0){
+    //       this.hungerLevel + 1;
+    //       }
+    //       if(this.boredom === 10)
+
+    //       $('#timer').text('Time: ', + this.time + 's');
+    //       $('#hunger').text("Hunger:( " + this.time + " )")
+    //     }, 1000)
+    //     $('#timer').append(this.time);
